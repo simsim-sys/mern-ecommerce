8b5e395db9ea8fd9d7a660a6a04c1b981cc6c5b8
PIANG <piang@PIANG-MACBOOK-PRO.local>
2026-05-26 11:49:34 +0630

    fix cors and add orders route

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://mern-ecommerce-six-eta.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -16,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
