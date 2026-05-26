738dbce3d1d3e51665832636686419928a0ab2ef
PIANG <piang@PIANG-MACBOOK-PRO.local>
2026-05-26 11:51:45 +0630

    fix cors properly

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,16 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'https://mern-ecommerce-six-eta.vercel.app',
+  origin: [
+    'https://mern-ecommerce-six-eta.vercel.app',
+    'http://localhost:5173'
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+app.options('*', cors());
 
 app.use(express.json());
 
